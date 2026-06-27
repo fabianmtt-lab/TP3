6e50b7b0ecd5d848a2fd641ea630ad4ad06f127d
Fabian Mattern <fabianmtt1221@gmail.com>
2026-06-26 23:40:03 -0300

    flexbox 2

---
+++ flexbox.js
@@ -0,0 +1,25 @@
+const datos = ["Elemento 1", "Elemento 2", "Elemento 3", "Elemento 4", "Elemento 5", "Elemento 6"];
+const elementos = [...datos];
+
+const contenedor = document.getElementById('contenedor-flex');
+const boton = document.getElementById('btn-interaccion');
+
+function renderizar() {
+
+    contenedor.innerHTML = '';
+
+
+    elementos.forEach(item => {
+        const div = document.createElement('div');
+        div.textContent = item;
+
+        contenedor.appendChild(div);
+    })
+}
+
+boton.addEventListener('click', () => {
+    elementos.reverse();
+    renderizar();
+});
+
+renderizar();
